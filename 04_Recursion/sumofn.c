c5a8d0c49ba716aac15d2c33a2ce033093207ddd
ilyasAbdi01 <ilyasyr@gmail.com>
2023-01-23 15:40:10 +0300

    second time

--- 04_Recursion/sumofn.c
+++ 04_Recursion/sumofn.c
@@ -1,16 +1,21 @@
 #include <stdio.h>
 int sum(int n);
 
+int factorial(int n);
+
 int main()
 {
-  int number, result;
+  int number, result, result2;
+  
 
   printf("enter a positive integer:");
   scanf("%i", &number);
 
   result = sum(number);
+  result2 = factorial (number);
 
-  printf("sum= %d\n", result);
+  printf("sum = %d\n", result);
+  printf("factorial = %d\n", result2);
   return 0;
 }
 
@@ -21,4 +26,12 @@ int sum(int n)
   return n + sum(n - 1);
   else 
       return n;
+}
+
+int factorial(int n)
+{
+  if (n == 0)
+    return 1;
+  return n * factorial(n - 1);
+
 }
